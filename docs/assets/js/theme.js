e7528e66b1d726fcbbf2a67494859d4e6c9e3844
KitsuneSolar <kitsune.solar@gmail.com>
2020-01-23 20:05:50 +0300

    2020-01-23 17:05:50

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,10 +1,18 @@
 'use strict';
 
+function clipBoard2(element = '.ext-clipboard') {
+  let clipboard = new ClipboardJS(element);
+
+  clipboard.on('success', function (e) {
+    e.clearSelection();
+  });
+}
+
 /**
  * Loading functions.
  * ---------------------------------------------------------------------------------------------------------------------
  */
 
 $(function () {
-	clipBoard('#chatUrl');
+	clipBoard2();
 });
